7997a3a11b2540ee0c1cbd416ac9898aa7fa8a22
JackyLin22 <jackylin5451@yahoo.com>
2021-09-23 18:34:33 -0400

    Lab 4 changes

---
+++ client/lab_4/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let slidePosition = 0;
+    const slides = document.querySelectorAll('.carousel_item');
+    const totalSlides = slides.length;
+
+    document.
+    querySelector("#next-button")
+    .addEventListener("click", function() {
+        moveToNextSlide();
+    });
+    document.
+    querySelector("#prev-button")
+    .addEventListener("click", function() {
+        moveToPrevSlide();
+    });
+
+    function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel_item--visible');
+        slide.classList.add('carousel_item--hidden');
+    }
+
+    slides[slidePosition].classList.add('carousel_item--visible');
+    }
+
+    function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+    }
+
+    function moveToPrevSlide() {
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+
+    updateSlidePosition();
+    }
+})
