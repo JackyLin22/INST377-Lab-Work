59ce55fc731d0d0c6314fcb3afa444d0d3955d51
JackyLin22 <jackylin5451@yahoo.com>
2021-10-08 23:32:48 -0400

    More changes

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,43 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
+async function windowActions() {
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-  const cities = [];
+  const request = await fetch(endpoint);
+  const cities = await request.json();
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestion');
+  document.querySelector('#search-result').innerHTML = '';
 
   fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => cities.push(...data));
+    .then((blob) => blob.json())
+    .then((data) => cities.push(...data));
 
   function findMatches(wordToMatch, cities) {
-    return cities.filter(place => {
+    return cities.filter((place) => {
       const regex = new RegExp(wordToMatch, 'gi');
       return place.city.match(regex) || place.state.match(regex);
     });
   }
 
-  function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3}) + (?!\d))/g, ',');
-  }
+  // function numberWithCommas(x) {
+  //   return x.toString().replace(/\B(?=(\d{3}) + (?!\d))/g, ',');
+  // }
 
-  function displayMatches() {
-    const matchArray = findMatches(this.value, cities);
-    const html = matchArray.map(place => {
-      const regex = new RegExp(this.value, 'gi');
-      const cityName = place.city.replace(regex, `<span class = h1>$
-      {this.value}</span>`);
-      const stateName = place.state.replace(regex, `<span class = h1>$
-      {this.value}</span>`);
-      return `
-        <li>
-          <span class = "name">${cityName}, ${stateName}</span>
-          <span class = "population">${numberWithCommas(place.population)}</span>
-        </li>
-      `;
-    }).join('');
+  function displayMatches(event) {
+    console.log('match val', event.value);
+    const matchArray = findMatches(event.target.value, cities);
+    const html = matchArray.map(
+      (search) => `<li>
+      ${search.name}
+      <br>${search.category}
+          <br>${search.address_line_1}
+          <br>${search.city}
+          <br>${search.zip}<br>
+          </span></li>`
+    ).join('');
     suggestions.innerHTML = html;
+    console.log('matches', matchArray);
   }
-
-  const searchInput = document.querySelector('.search');
-  const suggestions = document.querySelector('suggestions');
 
   searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', displayMatches);
-});
+  searchInput.addEventListener('keyup', (evt) => {
+    if (searchInput.value === '' || searchInput.value === undefined) {
+      suggestions.innerHTML = 0;
+      suggestions.innerHTML = 'You have found some results.';
+    } else {
+      displayMatches(evt);
+    }
+  });
+}
+
+window.onload = windowActions;
